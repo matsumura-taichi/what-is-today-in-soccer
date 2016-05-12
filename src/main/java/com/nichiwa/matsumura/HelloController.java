bbe8f7d2d73a18c7cd771979b592013d9581d97c
matsumura-taichi <matsumura.taichi@nichiwa-system.co.jp>
2016-05-12 19:21:40 +0900

    Thymeleafテンプレート導入

--- src/main/java/com/nichiwa/matsumura/HelloController.java
+++ src/main/java/com/nichiwa/matsumura/HelloController.java
@@ -1,15 +1,15 @@
 package com.nichiwa.matsumura;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "hello world!!";
+    public String hello(Model model) {
+        model.addAttribute("msg","It's a Thymeleaf sample!");
+        return "hello";
     }
 }
