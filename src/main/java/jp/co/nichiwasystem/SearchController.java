b21be2883a206c4d3e351ce6419720a090eb2222
matsumura-taichi <matsumura.taichi@nichiwa-system.co.jp>
2016-05-13 20:32:23 +0900

    MySQLに接続して、人物情報の登録・検索・表示を実装

---
+++ src/main/java/jp/co/nichiwasystem/SearchController.java
@@ -0,0 +1,110 @@
+package jp.co.nichiwasystem;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Controller
+@EnableAutoConfiguration
+public class SearchController
+{
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello, Spring Boot Sample Application!";
+    }
+
+    // an entry point
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SearchController.class, args);
+    }
+
+    @RequestMapping("/search")
+    public String search(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "search";
+    }
+
+ // input formを表示
+    @RequestMapping("/input")
+    public String input() {
+        return "input"; // input form
+    }
+
+    // inputフォームから受け取ってhello.htmlへ
+    @RequestMapping("/send")
+    public String send(Model model, @RequestParam("name") String name) {
+      model.addAttribute("name", name);
+      return "search";    // View file is templates/hello.html
+    }
+
+    // 日本語文字化け対策
+    @Bean
+    public Filter characterEncodingFilter() {
+      CharacterEncodingFilter filter = new CharacterEncodingFilter();
+      filter.setEncoding("UTF-8");
+      filter.setForceEncoding(true);
+      return filter;
+    }
+
+    @Autowired
+    PersonRepository repository;
+
+    @RequestMapping("/person-view")
+    public String personView(Model model) {
+      Iterable<Person> list = repository.findAll();
+      model.addAttribute("results", list);
+      return "person-view";
+    }
+
+    @RequestMapping(value="/post", method=RequestMethod.POST)
+    public String personSearch(Model model,
+      @RequestParam("name") String name,
+      @RequestParam("tel") String tel,
+      @RequestParam("mail") String mail,
+      @RequestParam("description") String description) {
+
+        Person person = new Person(name, tel, mail, description);
+        repository.saveAndFlush(person);
+        Iterable<Person> list = repository.findAll();
+        model.addAttribute("results", list);
+        return "person-view";
+    }
+
+    @RequestMapping(value="/find", method=RequestMethod.POST)
+    public String find(Model model,  @RequestParam("category") String category , @RequestParam("str") String str) {
+
+      Iterable<Person> list = null;
+
+      if (category.equals("name")) {
+        list = repository.findByName(str);
+      }
+      else if (category.equals("tel")) {
+        list = repository.findByTel(str);
+      }
+      else if (category.equals("mail")) {
+        list = repository.findByMail(str);
+      }
+      else if (category.equals("description")) {
+        list = repository.findByDescription(str);
+      }
+      else {
+        list = null;
+      }
+
+      model.addAttribute("results", list);
+      return "person-view";
+    }
+
+}
