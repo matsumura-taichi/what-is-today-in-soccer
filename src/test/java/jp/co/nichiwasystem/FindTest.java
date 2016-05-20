aadf20bc9569bbccc363e7507631002ba588c905
matsumura-taichi <matsumura.taichi@nichiwa-system.co.jp>
2016-05-20 15:13:54 +0900

    FindTest修正、赤のまま

--- src/test/java/jp/co/nichiwasystem/FindTest.java
+++ src/test/java/jp/co/nichiwasystem/FindTest.java
@@ -8,11 +8,11 @@ public class FindTest {
 
 	@Test
 	public void FindData() {
-		assertEquals(new SearchController().FindDataCount(1, 1), new SearchController().FindDataCount(1, 1));
+		assertEquals(1, new SearchController().FindDataCount(1, 1));
     }
 
-//	@Test
-//	public void NotFindData() {
-//		assertEquals(0, new SearchController().FindDataCount(2, 3));
-//    }
+	@Test
+	public void NotFindData() {
+		assertEquals(0, new SearchController().FindDataCount(2, 3));
+    }
 }
